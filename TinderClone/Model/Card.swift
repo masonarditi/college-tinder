e7962ed3b978c254b760a9d12b96a6cb431adf40
Mason Arditi <masonarditi@berkeley.edu>
2025-03-09 15:23:12 -0700

    update Card schema for new db info

--- TinderClone/Model/Card.swift
+++ TinderClone/Model/Card.swift
@@ -7,4 +7,17 @@ struct Card: Identifiable, Codable {
     let year: Int
     let desc: String
     let imageURL: String
+    
+    // new info
+    
+    let athletics: Int
+    let diningRating: Int
+    let greekLife: Int
+    let institutionType: String
+    let location: String
+    let overall: Int
+    let ranking: Int
+    let studentPopulation: Int
+    let topMajors: [String]
+    let website: String
 }
